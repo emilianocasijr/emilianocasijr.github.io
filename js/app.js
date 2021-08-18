b636f198f27fae6fe56cbba5b5808993a431cde8
emilianocasijr <casi.emilianojr@gmail.com>
2021-08-19 00:56:38 +0800

    Fixed form, about me and smooth scroll

--- js/app.js
+++ js/app.js
@@ -20,28 +20,45 @@ const observer = new IntersectionObserver(
             } else if(entry.target.classList.contains("introduction")){
                 body.style.backgroundColor="#151515";
                 navProjects.classList.remove("current-page");
+                navSkills.classList.remove("current-page");
+                navContactMe.classList.remove("current-page");
+                navAboutMe.classList.remove("current-page");
             } else if(entry.target.classList.contains("project1")){
                 body.style.backgroundColor="#0F0C00";
                 nav.style.transform="translate(0, -50%)";
                 navProjects.classList.add("current-page");
+                navSkills.classList.remove("current-page");
+                navContactMe.classList.remove("current-page");
+                navAboutMe.classList.remove("current-page");
             } else if(entry.target.classList.contains("project2")){
                 body.style.backgroundColor="#150D24";
             } else if(entry.target.classList.contains("project3")){
                 body.style.backgroundColor="#01060C";
                 navSkills.classList.remove("current-page");
+                navContactMe.classList.remove("current-page");
+                navAboutMe.classList.remove("current-page");
                 navProjects.classList.add("current-page");
             } else if(entry.target.classList.contains("skills")){
                 body.style.backgroundColor="#151515";
                 navProjects.classList.remove("current-page");
-                navSkills.classList.add("current-page");
                 navContactMe.classList.remove("current-page");
+                navAboutMe.classList.remove("current-page");
+                navSkills.classList.add("current-page");
                 skills.forEach(skill => {
                     skill.classList.add("fade-in");
                 })
             } else if(entry.target.classList.contains("contact-me")){
                 body.style.backgroundColor="#303E4F";
+                navProjects.classList.remove("current-page");
                 navSkills.classList.remove("current-page");
+                navAboutMe.classList.remove("current-page");
                 navContactMe.classList.add("current-page");
+            } else if(entry.target.classList.contains("about-me")){
+                body.style.backgroundColor="#151515";
+                navProjects.classList.remove("current-page");
+                navSkills.classList.remove("current-page");
+                navContactMe.classList.remove("current-page");
+                navAboutMe.classList.add("current-page");
             }
         })
     }, options);
@@ -50,3 +67,29 @@ sections.forEach(
     section => {
         observer.observe(section);
 })
+
+
+
+// CONTACT ME FORM from "formspree"
+var form = document.getElementById("contact-me-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("contact-me-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Message successfully sent. Thank you!";
+    status.classList.add("success")
+    form.reset()
+  }).catch(error => {
+    status.classList.add("error")
+    status.innerHTML = "Oops! Message not sent. Please contact me through my email."
+  });
+}
+form.addEventListener("submit", handleSubmit)
